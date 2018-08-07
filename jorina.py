3735afeea54d3bcd8a9e408cfeb5e309cdddecbc
Imtiaz Ahmed <imtiaz.ahmed.ratul@gmail.com>
2018-08-06 22:33:53 -0600

    initiated weather, minor tweaks

--- jorina.py
+++ jorina.py
@@ -3,7 +3,7 @@
 # Jorina is a command line assistant
 # Version 1.0
 
-import sys
+import sys, json, requests
 from modules import *
 
 if len(sys.argv) < 2:
@@ -11,6 +11,16 @@ if len(sys.argv) < 2:
 elif sys.argv[1] == "man":
     manual()
 elif sys.argv[1] == "convert":
-    convert(sys.argv[2], sys.argv[3])
+    try:
+        convert(sys.argv[2], sys.argv[3])
+    except IndexError:
+        error()
+elif sys.argv[1] == "weather":
+    if len(sys.argv) == 2:
+        city = "Salt Lake City"
+        weather(city)
+    elif len(sys.argv) > 2 and sys.argv[2] == "city":
+        city = input("Enter city name: ")
+        weather(city)
 else:
     error()
--- modules.py
+++ modules.py
@@ -1,20 +1,24 @@
+# Error code
 def error():
     print("\n---- Jorina ----")
+    print("Error 101")
     print("Usage: <jorina 'module'>")
     print("Example: <jorina weather>")
     print("For list of usage commands, enter <jorina man>\n")
 
+# Manual
 def manual():
     print("\n---- Jorina ----")
     print("Jorina supports weather, football and conversion at the moment.")
     print("Weather for current city: <jorina weather>")
-    print("Weather for specific city: <jorina weather 'city name'>")
+    print("Weather for specific city: <jorina weather city>")
     print("Football fixture for certain club: <jorina football 'club name'>")
     print("(Fixture is for last game and next game only)")
     print("Conversion supports: lbs to kg, dollar to tk and miles to km")
     print("Conversion usage: <jorina convert 'unit-unit' '0000'>")
     print("Example: <jorina convert lbs-kg 23>\n")
 
+# Unit Converter
 def convert(unit, value):
     if unit == "lbs-kg":
         print("\n---- Jorina ----")
@@ -35,3 +39,7 @@ def convert(unit, value):
         print("\n---- Jorina ----")
         print("Invalid instructions. ")
         print("For list of usage commands, enter <jorina man>\n")
+
+# Weather
+def weather(city):
+    return 0
